470d4b79aa7a8303c8823576a9114e0a6012df87
sofiaelded <126705432+sofiaelded@users.noreply.github.com>
2023-05-09 13:59:41 -0300

    Add files via upload

---
+++ ejercicio42.py
@@ -0,0 +1,10 @@
+a=10
+suma=0
+promedio=0
+for x in range (a):
+    num=float(input("Ingrese 10 valores: "))
+    suma=suma+num
+    promedio=suma/10
+print("La suma de todos los valores es: ",suma)
+print("El promedio de todos los valores es: ",promedio)
+        
---
+++ ejercicio42while.py
@@ -0,0 +1,11 @@
+a=10
+suma=0
+promedio=0
+x=0
+while x<10:
+    num=float(input("Ingrese 10 valores: "))
+    suma=suma+num
+    promedio=suma/10
+    x=x+1
+print("La suma de todos los valores es: ",suma)
+print("El promedio de todos los valores es: ",promedio)
