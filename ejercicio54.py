be9816506235e56302f1bb52dc55aff8b7cbccc7
sofiaelded <126705432+sofiaelded@users.noreply.github.com>
2023-05-23 13:25:10 -0300

    Add files via upload

---
+++ ejercicio54.py
@@ -0,0 +1,6 @@
+clave= input("Ingrese una clave: ")
+longitud = len(clave)
+if 10 <= longitud <= 20:
+    print("La clave es válida.")
+else:
+    print("La clave debe tener entre 10 y 20 caracteres.")
